4218b90c2240fd4bcf28bc836dfe438ffae424f6
Jewels <jewelsytoffcial@gmail.com>
2024-10-07 20:33:38 -0400

    creating GUI

---
+++ GTC/Functions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terminal.Gui;
+
+namespace GTC
+{
+	internal static class Functions
+	{
+		public static void ResizeScrollView(ref ScrollView view)
+		{
+			int w = 0;
+			int h = 0;
+			foreach (View v in view.Subviews)
+			{
+				w = v.Frame.Width > w ? v.Frame.Width : w;
+				h += v.Frame.Height;
+			}
+			view.ContentSize = new(w, h);
+		}
+
+		public static int AboveZero(int i)
+		{
+			if (i > 0) return i;
+			else return 0;
+		}
+	}
+}
--- GTC/MainWindow.cs
+++ GTC/MainWindow.cs
@@ -8,13 +8,50 @@
 //  </auto-generated>
 // -----------------------------------------------------------------------------
 namespace GTC {
-    using Terminal.Gui;
-    
-    
-    public partial class MainWindow {
-        
-        public MainWindow() {
-            InitializeComponent();
-        }
-    }
+	using Terminal.Gui;
+	using static GTC.Functions; 
+	
+	public partial class MainWindow 
+	{
+		private int messagesY = 0;	
+
+		public MainWindow() {
+			InitializeComponent();
+			Initialize();
+
+			AddMessage("Hello");
+			AddMessage("Hello 12345677889900987654312345678909876553221");
+		}
+
+		private void Initialize()
+		{
+			messagesView.ShowVerticalScrollIndicator = true;
+			messagesView.ShowHorizontalScrollIndicator = true;
+
+			textView.KeyUp += (k) =>
+			{
+				if (k.KeyEvent.Key == Key.Enter)
+				{
+					AddMessage((string)textView.Text.Substring(0, textView.Text.Length - 1));
+					textView.Text = "";
+				}
+			};
+		}
+
+		private void AddMessage(string msg)
+		{
+			Label label = new Label(msg)
+			{
+				X = 0,
+				Y = messagesY,
+			};
+			messagesView.ContentSize = new(
+				messagesView.ContentSize.Width + AboveZero(messagesView.ContentSize.Width - label.Bounds.Width), 
+				messagesView.ContentSize.Height + label.Bounds.Height
+			);
+			label.Width = Dim.Fill();
+			messagesView.Add(label);
+			messagesY += label.Bounds.Height;
+		}
+	}
 }
